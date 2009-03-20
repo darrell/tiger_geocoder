753b48b19930591ad2a591130bb5f34a84c55af6
Darrell Fuhriman <darrell@havisham.dyn.projectdx.com>
2009-03-20 10:10:40 -0700

    add more possible state abbreviations

--- tables/state_lookup.sql
+++ tables/state_lookup.sql
@@ -1,122 +1,72 @@
    set search_path=tiger,public;
    set client_min_messages = error;
   DROP TABLE IF EXISTS state_lookup cascade;
-CREATE TABLE state_lookup (statefp varchar(2) PRIMARY KEY, name VARCHAR(40) UNIQUE, abbrev VARCHAR(3) UNIQUE);
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Alabama', 'AL', '01');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Alaska', 'AK', '02');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('American Samoa', 'AS', -1);
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Arizona', 'AZ', '04');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Arkansas', 'AR', '05');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('California', 'CA', '06');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Colorado', 'CO', '08');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Connecticut', 'CT', '09');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Delaware', 'DE', '10');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('District of Columbia', 'DC', '11');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Federated States of Micronesia', 'FM', -2);
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Florida', 'FL', '12');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Georgia', 'GA', '13');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Guam', 'GU', -7);
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Hawaii', 'HI', '15');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Idaho', 'ID', '16');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Illinois', 'IL', '17');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Indiana', 'IN', '18');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Iowa', 'IA', '19');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Kansas', 'KS', '20');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Kentucky', 'KY', '21');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Louisiana', 'LA', '22');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Maine', 'ME', '23');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Marshall Islands', 'MH', -3);
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Maryland', 'MD', '24');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Massachusetts', 'MA', '25');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Michigan', 'MI', '26');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Minnesota', 'MN', '27');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Mississippi', 'MS', '28');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Missouri', 'MO', '29');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Montana', 'MT', '30');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Nebraska', 'NE', '31');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Nevada', 'NV', '32');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('New Hampshire', 'NH', '33');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('New Jersey', 'NJ', '34');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('New Mexico', 'NM', '35');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('New York', 'NY', '36');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('North Carolina', 'NC', '37');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('North Dakota', 'ND', '38');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Northern Mariana Islands', 'MP', -4);
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Ohio', 'OH', '39');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Oklahoma', 'OK', '40');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ( 'Oregon', 'OR' , '41');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Palau', 'PW', -5);
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Pennsylvania', 'PA', '42');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Puerto Rico', 'PR', '72');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Rhode Island', 'RI', '44');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('South Carolina', 'SC', '45');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('South Dakota', 'SD', '46');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Tennessee', 'TN', '47');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Texas', 'TX', '48');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Utah', 'UT', '49');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Vermont', 'VT', '50');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Virgin Islands', 'VI', -6);
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Virginia', 'VA', '51');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Washington', 'WA', '53');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('West Virginia', 'WV', '54');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Wisconsin', 'WI', '55');
-INSERT INTO state_lookup (name, abbrev, statefp)
-VALUES ('Wyoming', 'WY', '56');
+CREATE TABLE state_lookup (
+  statefp varchar(2) PRIMARY KEY,
+  name VARCHAR(40) UNIQUE,
+  abbrev VARCHAR(3) UNIQUE,
+  gpoabbrev varchar(10) UNIQUE,
+  altabbrev varchar(10) UNIQUE);
+
+COPY state_lookup FROM STDIN;
+01	Alabama	AL	Ala.	Ala.
+02	Alaska	AK	Alaska	Alaska
+04	Arizona	AZ	Ariz.	Ariz.
+05	Arkansas	AR	Ark.	Ark.
+06	California	CA	Calif.	Calif.
+08	Colorado	CO	Colo.	Colo.
+09	Connecticut	CT	Conn.	Conn.
+10	Delaware	DE	Del.	Del.
+11	District of Columbia	DC	D.C.	D.C.
+12	Florida	FL	Fla.	Fla.
+13	Georgia	GA	Ga.	Ga.
+15	Hawaii	HI	Hawaii	Hawaii
+16	Idaho	ID	Idaho	Idaho
+17	Illinois	IL	Ill.	Ill.
+18	Indiana	IN	Ind.	Ind.
+19	Iowa	IA	Iowa	Iowa
+20	Kansas	KS	Kans.	Kan.
+21	Kentucky	KY	Ky.	Ky.
+22	Louisiana	LA	La.	La.
+23	Maine	ME	Maine	Maine
+24	Maryland	MD	Md.	Md.
+25	Massachusetts	MA	Mass.	Mass.
+26	Michigan	MI	Mich.	Mich.
+27	Minnesota	MN	Minn.	Minn.
+28	Mississippi	MS	Miss.	Miss.
+29	Missouri	MO	Mo.	Mo.
+30	Montana	MT	Mont.	Mont.
+31	Nebraska	NE	Nebr.	Neb.
+32	Nevada	NV	Nev.	Nev.
+33	New Hampshire	NH	N.H.	N.H.
+34	New Jersey	NJ	N.J.	N.J.
+35	New Mexico	NM	N. Mex.	N.M.
+36	New York	NY	N.Y.	N.Y.
+37	North Carolina	NC	N.C.	N.C.
+38	North Dakota	ND	N. Dak.	N.D.
+39	Ohio	OH	Ohio	Ohio
+40	Oklahoma	OK	Okla.	Okla.
+41	Oregon	OR	Oreg.	Ore.
+42	Pennsylvania	PA	Pa.	Pa.
+44	Rhode Island	RI	R.I.	R.I.
+45	South Carolina	SC	S.C.	S.C.
+46	South Dakota	SD	S. Dak.	S.D.
+47	Tennessee	TN	Tenn.	Tenn.
+48	Texas	TX	Tex.	Texas
+49	Utah	UT	Utah	Utah
+50	Vermont	VT	Vt.	Vt.
+51	Virginia	VA	Va.	Va.
+53	Washington	WA	Wash.	Wash.
+54	West Virginia	WV	W. Va.	W.Va.
+55	Wisconsin	WI	Wis.	Wis.
+56	Wyoming	WY	Wyo.	Wyo.
+60	American Samoa	AS	A.S.	\N
+64	Micronesia	FM	\N	\N
+66	Guam	GU	Guam	\N
+68	Marshall Islands	MH	\N	\N
+69	Northern Mariana Islands	MP	M.P.	\N
+70	Palau	PW	\N	\N
+72	Puerto Rico	PR	P.R.	\N
+78	Virgin Islands	VI	V.I.	\N
+\.
+
