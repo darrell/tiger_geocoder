83190907b9ceb04ba73dd83c2bf080b9a40a0d58
Darrell Fuhriman <darrell@artful.garnix.org>
2009-05-08 19:32:04 -0700

    added initial street address parsing, modeled on pyparsing examples.

--- address_parser/address_parser.py
+++ address_parser/address_parser.py
@@ -4,6 +4,21 @@ from pyparsing import *
 
 complicated_string='XyZzY'
 original_state=''
+def flatten(l, ltypes=(list, tuple)):
+    ltype = type(l)
+    l = list(l)
+    i = 0
+    while i < len(l):
+        while isinstance(l[i], ltypes):
+            if not l[i]:
+                l.pop(i)
+                i -= 1
+                break
+            else:
+                l[i:i + 1] = l[i]
+        i += 1
+    return ltype(l)
+  
 def getStates():
   def makeState(s,arr):
     ret=[]
@@ -27,6 +42,14 @@ def getStates():
 #  st=Or(states.values())
   return Or(states.values()).setResultsName('state')
 
+def getPlaces():
+  places=[]
+  with open("places.txt") as pl:
+    for line in pl:
+      places.append(CaselessLiteral(line.strip()))
+  return Or(places).setResultsName('place')
+
+
 def test(p,s,expected):
   failed=False
   try:
@@ -46,8 +69,8 @@ def test(p,s,expected):
           failed=True
           print "   '%s' -> '%s' ***WRONG***  expected '%s'" % (k,result.get(k),v)
       if failed:
-        print "   TOKENS are %s" % result.asDict()
-        #print result.asList
+        print "TOKENS are:" #"%s" % result.asDict()
+        print result.dump()
           
         #else:
           #print "***WRONG***, expected %s for %s" % (key,value)
@@ -58,6 +81,9 @@ def test(p,s,expected):
         print "CORRECT"
       else:
         print "***WRONG***, expected %s" % result.asList
+    if expected=={}:
+      print result.dump()
+  print "-----------"
 
   
 # Zipcodes
@@ -66,20 +92,65 @@ zip5 = Word(nums, exact=5).setResultsName('zip5')
 zip4 = Combine(Optional(zipsep,'-') + Word(nums, exact=4))+LineEnd()
 zip4.setResultsName('zip4')
 #zip4.setDebug( True )
-zipcode = Combine(zip5.setResultsName('zip') +Optional(zip4.setResultsName('zip4'))).setParseAction(lambda toks: "x".join(toks[0])).setResultsName('zipcode')
+zipcode = Combine(zip5.setResultsName('zip') +Optional(zip4.setResultsName('zip4'))).setParseAction(lambda toks: "".join(toks[0])).setResultsName('zipcode')
 #zipcode = Regex('\d{5}[-+]{0,1}\d{4}').setResultsName('zipcode')
 
 # States
 state=getStates()
 
-# Streets
+# address component separator
 addrSep=ZeroOrMore(White())+ZeroOrMore(Literal(','))+ZeroOrMore(White()).suppress()
 
 #Places
-place= Combine(OneOrMore(Word(alphas+'.')+Optional(White()))).setResultsName('place')
-#place.setDebug(True)
+try:
+  place = getPlaces()
+except:
+  place= Combine(OneOrMore(Word(alphas+'.')+Optional(White()))).setResultsName('place')
+
+place=place.setResultsName('place')
+#print place
+#House Number & street
+# began with code cribbed from pyparsing example code at
+# http://pyparsing.wikispaces.com/file/view/streetAddressParser.py
+# number can be any of the forms 123, 21B, or 23 1/2
+number = ( Combine(Word(nums) + 
+                   Optional(oneOf(list(alphas))+FollowedBy(White()))) + \
+            Optional(Optional("-") + "1/2")
+         ).setParseAction(keepOriginalText, lambda t:t[0].strip())
+
+word = Word(alphas+'.'+'-')
+#words = Group(OneOrMore(~Literal("Ave")+Word(alphas)))+"Ave"
+streetNumberSuffix = oneOf("st th nd", caseless=True).setResultsName("numbersuffix")
+
+# numberedStreet = ~streetNumberSuffix+
+numberedStreet = (OneOrMore(Word(nums))+streetNumberSuffix).setParseAction(lambda toks: "".join(toks)).setResultsName("numberedstreet")
+
+# types of streets - extend as desired
+predir = oneOf("N S E W NE SE NW SW", caseless=True).setResultsName('predir')
+
+predir.setParseAction(lambda t:t[0].strip())
+suftype = Combine( oneOf("Street St Boulevard Blvd Lane Ln Road Rd Avenue Ave "
+                        "Circle Cir Cove Cv Drive Dr Parkway Pkwy Court Ct",
+                        caseless=True) + Optional(".").suppress()).setResultsName("suftype")
+
+# <streetname> ::== <word>+ | <digit>+<numbersuffix>*;
+streetname = (Group(OneOrMore(~suftype+Word(alphas))).setParseAction(lambda toks: " ".join(toks[0]))|numberedStreet)
+
+# <street> ::== <predir>? <whitespace> <pretyp>? <whitespace> <prequal>? <whitespace> 
+#               <streetname> <whitespace> 
+#               <suftype>? <whitespace> <sufdir>? <whitespace>  <sufqual>?;
+
+street=Optional(predir("predir"))+streetname("streetname")+ZeroOrMore(suftype)
+
+address = number.setResultsName("housenumber") + street
+address.setResultsName('address')
+
+intersection = ( street.setResultsName("intersectionA").setParseAction(lambda toks: " ".join(toks)) + 
+                 oneOf('and & at',caseless=True) +
+                 street.setResultsName("intersectionB").setParseAction(lambda toks: " ".join(toks)) )
+
 # Whole Address
-address= Optional(place) + Optional(addrSep) + Optional(state) + Optional(addrSep).suppress() + Optional(zipcode)
+location= Optional(street)+Optional(addrSep)+Optional(place) + Optional(addrSep) + Optional(state) + Optional(addrSep).suppress() + Optional(zipcode)
 
 
 test(zipsep, '+', '-')
@@ -92,14 +163,23 @@ test(zipcode,'972141234', '97214-1234')
 test(zipcode,'97214-','97214')
 test(state,'Oregon', 'OR')
 test(state,'Ore., 97212-1234','OR')
-test(address,'Ore., 97212',{'state': 'OR', 'zipcode': '97212'})
-test(address,'Ore., 97212-1234',{'state': 'OR', 'zipcode': '97212-1234'})
-test(address, "Portland, Oregon", {'place': 'Portland', 'state': 'OR'})
-test(address, "Portland, Ore. 97212-1234",{'place': 'Portland','state': 'OR', 'zipcode': '97212-1234'})
-test(address, "Foo City, KS 12345-1234",{'place': 'Foo City','state': 'KS', 'zipcode': '12345-1234'})
-test(address, "Foo City, 12345-1234",{'place': 'Foo City', 'zipcode': '12345-1234'})
+test(location,'Ore., 97212',{'state': 'OR', 'zipcode': '97212'})
+test(location,'Ore., 97212-1234',{'state': 'OR', 'zipcode': '97212-1234'})
+test(location, "Portland, Oregon", {'place': 'Portland', 'state': 'OR'})
+test(location, "Portland, Ore. 97212-1234",{'place': 'Portland','state': 'OR', 'zipcode': '97212-1234'})
+test(location, "Oregon City, KS 12345-1234",{'place': 'Oregon City','state': 'KS', 'zipcode': '12345-1234'})
+test(location, "Oregon City, 12345-1234",{'place': 'Oregon City', 'zipcode': '12345-1234'})
 #address.setDebug(True)
-test(address, "Oregon City, KS 12345-1234", {'place': 'Oregon City','state': 'KS', 'zipcode': '12345-1234'})
-test(address, "Oregon City, 12345-1234", {'place': 'Oregon City', 'zipcode': '12345-1234'})
-test(address, "Oregon City, Oregon, 12345-1234", {'place': 'Oregon City', 'zipcode': '12345-1234'})
-test(address, "Oregon, 12345-1234", {'state': 'OR', 'zipcode': '12345-1234'})
+test(location, "Oregon City, KS 12345-1234", {'place': 'Oregon City','state': 'KS', 'zipcode': '12345-1234'})
+test(location, "Oregon City, 12345-1234", {'place': 'Oregon City', 'zipcode': '12345-1234'})
+test(location, "Oregon City, Oregon, 12345-1234", {'place': 'Oregon City', 'zipcode': '12345-1234'})
+test(location, "Oregon, 12345-1234", {'state': 'OR', 'zipcode': '12345-1234'})
+test(address, "123 Main St.", {'housenumber': '123', 'streetname':'Main', 'suftype': 'St'})
+test(street, "21St St.", {'suftype': 'St','streetname': '21st'})
+test(street, "Pale Horse Ave",{'streetname': 'Pale Horse', 'suftype': 'Ave'})
+test(street, "SE Pale Horse Ave",{'streetname': 'Pale Horse', 'suftype': 'Ave', 'predir': 'SE'})
+test(street, "SE 21St Ave.", {'predir': 'SE','suftype': 'Ave','streetname': '21st'})
+test(street, "SE 21St St.", {'predir': 'SE','suftype': 'St','streetname': '21st'})
+test(intersection, "NE Foo Rd and 21ST Ave", {'intersectionA': 'NE Foo Rd', 'intersectionB': '21st Ave'})
+test(address, "123 SE 21St St.", {'housenumber': '123', 'predir': 'SE','streetname': '21st', 'suftype': 'St'})
+test(location, "123 Main St, Portland, Oregon, 97212",{'place': 'Portland', 'state': 'OR', 'zipcode': '12345-1234'})
