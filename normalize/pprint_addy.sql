ab1fc6d4d5cabe81e47944bcd8c3e9853afbb5c1
Darrell Fuhriman <darrell@projectdx.com>
2009-08-10 15:46:13 -0700

    replace pprint with more compact version, with option to display internal portion of address. Added cast to text for norm_addy as well

--- normalize/pprint_addy.sql
+++ normalize/pprint_addy.sql
@@ -1,42 +1,47 @@
-CREATE OR REPLACE FUNCTION pprint_addy(
-    input NORM_ADDY
-) RETURNS VARCHAR
-AS $_$
-DECLARE
-  result VARCHAR;
-BEGIN
-  IF NOT input.parsed THEN
-    RETURN NULL;
-  END IF;
-
-  result := cull_null(input.address::text)
-         || CASE WHEN input.preDirAbbrev IS NOT NULL THEN ' ' ELSE '' END
-         || cull_null(upper(input.preDirAbbrev))
-         || CASE WHEN input.preTypeAbbrev IS NOT NULL THEN ' ' ELSE '' END
-         || cull_null(initcap(input.preTypeAbbrev))
-         || CASE WHEN input.preQualAbbrev IS NOT NULL THEN ' ' ELSE '' END
-         || cull_null(initcap(input.preQualAbbrev))
-         || CASE WHEN input.streetName IS NOT NULL THEN ' ' ELSE '' END
-         || cull_null(initcap(input.streetName))
-         || CASE WHEN input.streetTypeAbbrev IS NOT NULL THEN ' ' ELSE '' END
-         || cull_null(initcap(input.streetTypeAbbrev))
-         || CASE WHEN input.streetDirAbbrev IS NOT NULL THEN ' ' ELSE '' END
-         || cull_null(upper(input.streetDirAbbrev))
-         || CASE WHEN input.streetQualAbbrev IS NOT NULL THEN ' ' ELSE '' END
-         || cull_null(initcap(input.streetQualAbbrev))
-         || CASE WHEN
-              input.address IS NOT NULL OR
-              input.streetName IS NOT NULL
-              THEN ', ' ELSE '' END
-         || cull_null(initcap(input.internal))
-         || CASE WHEN input.internal IS NOT NULL THEN ', ' ELSE '' END
-         || cull_null(initcap(input.location))
-         || CASE WHEN input.location IS NOT NULL THEN ', ' ELSE '' END
-         || cull_null(upper(input.stateAbbrev))
-         || CASE WHEN input.stateAbbrev IS NOT NULL THEN ' ' ELSE '' END
-         || cull_null(input.zip);
-
-  RETURN trim(result);
-
+CREATE OR REPLACE FUNCTION pprint_addy(a norm_addy, show_internal boolean) RETURNS text AS $$
+  DECLARE
+   ret text;
+  BEGIN
+   IF NOT a.parsed THEN
+     RETURN NULL;
+   END IF;
+   ret:=array_to_string(ARRAY[a.address::text,upper(a.preDirAbbrev),initcap(a.preTypeAbbrev),initcap(a.preQualAbbrev),a.streetName,initcap(a.streetTypeAbbrev),upper(a.streetDirAbbrev),initcap(a.streetQualAbbrev)], ' '); 
+   IF show_internal AND a.internal IS NOT NULL THEN
+     ret := ret || ', ' || a.internal;
+   END IF;
+   IF  a.location IS NOT NULL THEN
+    ret := ret || ', ' || a.location;
+   END IF;
+   IF a.stateAbbrev IS NOT NULL THEN
+    ret := ret || ', ' || a.stateAbbrev;
+   END IF;
+   IF a.zip IS NOT NULL THEN
+     IF a.zip4 IS NOT NULL THEN
+      ret := ret || ' ' || a.zip || '-' || a.zip4;
+     ELSE
+      ret := ret || ' ' || a.zip;
+     END IF;
+   END IF;
+  RETURN ret;
 END;
-$_$ LANGUAGE plpgsql IMMUTABLE STRICT;
+$$ LANGUAGE plpgsql immutable strict;
+CREATE OR REPLACE FUNCTION pprint_addy(a norm_addy) RETURNS text AS $$
+  BEGIN
+  RETURN pprint_addy(a, true);
+  END;
+$$ LANGUAGE plpgsql immutable strict;
+
+DROP CAST(norm_addy as text) IF EXISTS;
+CREATE CAST(norm_addy as text)  WITH FUNCTION pprint_addy(norm_addy);
+
+COMMENT ON FUNCTION pprint_addy(a norm_addy) IS $$
+returns a formatted version of the parsed address. 
+Optional boolean as second argument tells whether or not
+to include the 'internal' portion of the address (default true).
+$$;
+COMMENT ON FUNCTION pprint_addy(norm_addy, boolean) IS $$
+returns a formatted version of the parsed address. 
+Optional boolean as second argument tells whether or not
+to include the 'internal' portion of the address (default true).
+$$;
+
